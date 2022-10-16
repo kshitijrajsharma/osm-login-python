dfb39148495da9f3b93f4b4ef606f2eba5e44b46
itskshitiz321 <skshitizraj@gmail.com>
2022-10-16 18:47:33 +0545

    added setup.py

--- auth/__init__.py
+++ auth/__init__.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
 
+
 class Login(BaseModel):
     url: str
+
 
 class Token(BaseModel):
     access_token: str
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+requirements = ["itsdangerous~=2.1.2", "pydantic~=1.10.2", "requests_oauthlib~=1.3.1"]
+
+setuptools.setup(
+    name="osm-login-python",
+    version="0.0.1",
+    author="Kshitij Raj Sharma",
+    author_email="kshitij.sharma@hotosm.org",
+    description="Use OSM Token exchange with OAuth2.0 for python projects",
+    license="BSD-3-Clause",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kshitijrajsharma/osm-login-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=requirements,
+    requires_python=">=3.0",
+)
