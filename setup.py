d57fa3288b7c6616eb7ddf1715c6314ffbc9416f
kshitijrajsharma <skshitizraj@gmail.com>
2023-09-02 08:02:10 +0545

    bump: version 0.0.4 → 1.0.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     name="osm-login-python",
     version="0.0.4",
     author="Kshitij Raj Sharma",
-    author_email="kshitij.sharma@hotosm.org",
+    author_email="skshitizraj@gmail.com",
     description="Use OSM Token exchange with OAuth2.0 for python projects",
     license="BSD-3-Clause",
     long_description=long_description,
